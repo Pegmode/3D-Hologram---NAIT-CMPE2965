2c62769025da8f69bad1fa6f2d624d8196e68ad9
Daniel_Chu <dchu4@nait.ca>
2026-01-26 10:34:19 -0700

    basic coord conversion test

--- server/tools/voxelLookupGenerator/voxelLookupGenerator.py
+++ server/tools/voxelLookupGenerator/voxelLookupGenerator.py
@@ -5,16 +5,32 @@ import numpy
 LED_DISTANCE = 10#
 
 ##functions
+##
+##Math functions
 def carteseian2Cylindrical(cartesianCoordinate):
-
+    '''
+    Take a cartesian coordinate (x,y,z) and convert it to a cylindrical coordinate (theta, r, h)
+    where theta is in radians 
+    
+    :param cartesianCoordinate: 3D touple of x, y, z cartesian coordinate
+    '''
     #theta = arctan2(y,x)
     #r = sqrt(x^2+y^2)
     #h = z
     x, y, z = cartesianCoordinate
     r = numpy.sqrt(x^2+y^2)
     theta = numpy.arctan2(y,x)#angle in radians
-    pass
+    return theta, r, z
     
+##LUT functions
+def debugConvertSystem2D():
+    cartesianSize = 30#
+    coordList = []
+    for y in range(cartesianSize):
+        for x in range(cartesianSize):
+            theta, r, _ = carteseian2Cylindrical((x,y,0))
+            coordList.append((theta, r))
+
 
 ##main
 if __name__ == "__main__":
