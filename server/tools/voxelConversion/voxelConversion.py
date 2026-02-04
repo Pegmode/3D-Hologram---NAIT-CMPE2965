6cc4e5430991fb2ce6b96d700677223e07fab1c8
Pegmode <danieljchu7@gmail.com>
2026-02-04 15:39:39 -0700

    skeleton voxel conversion

---
+++ server/tools/voxelConversion/voxelConversion.py
@@ -0,0 +1,10 @@
+##Converts something from the `VL332` format to something we can use in python
+#by Daniel Chu
+#
+# VL32 format spec: https://eisenwave.github.io/voxel-compression-docs/file_formats/vl32.html
+
+
+
+
+if __name__ == "__main__":
+    pass
