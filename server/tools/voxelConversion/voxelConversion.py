033c97771e49090008bc67714d52fb3c559c6bed
Pegmode <danieljchu7@gmail.com>
2026-02-04 15:47:30 -0700

    added voxel unpacking to voxel conversion tool

--- server/tools/voxelConversion/voxelConversion.py
+++ server/tools/voxelConversion/voxelConversion.py
@@ -2,9 +2,26 @@
 #by Daniel Chu
 #
 # VL32 format spec: https://eisenwave.github.io/voxel-compression-docs/file_formats/vl32.html
+import struct
+import matplotlib.pyplot as plt
 
+DEBUG_FILEPATH = "tea.vl32"
 
-
+def readVL32(path):
+    '''
+    Read a vl32 file and return a 3D list of coordinates (x, y, z)
+    
+    :param path: Path to .vl32 file
+    '''
+    voxels = []
+    with open(path, "rb") as f:
+        while True:
+            chunk = f.read(16) #format spec shows each voxel is 16 bytes 
+            if len(chunk) < 16:#check if there is tailing data at the end of the file
+                break
+            x, y, z, a, _, _, _ = struct.unpack(">iiiBBBB", chunk)# unpack the struct given in the format spec, ignore color data, keep alpha for pixel is present
+            if a != 0: #dont save the voxel if its see through / not present
+                voxels.append((x, y, z))
 
 if __name__ == "__main__":
     pass
